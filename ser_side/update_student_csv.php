33325510f8295802a5135997008ae930431a98be
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2018-09-18 10:16:31 +0700

    a

--- ser_side/update_student_csv.php
+++ ser_side/update_student_csv.php
@@ -7,12 +7,12 @@
     $i = 1;
 
     //update data form last to head
-    $a = "SELECT `order` FROM `student_table` ORDER BY `order` DESC";
+    $a = "SELECT `order`,`login_password` FROM `student_table` ORDER BY `order` DESC";
     $q_a = mysqli_query($con,$a);
-    while($i<$id){
-        $r_a = mysqli_fetch_assoc($q_a);
+    while($i<=$id&& $r_a = mysqli_fetch_assoc($q_a)){
+       
         $r_b = base64_encode($r_a['login_password']);
-        $b = "UPDATE login_password='$r_b' ";
+        $b = "UPDATE `student_table` SET login_password='$r_b' WHERE `order` = 'r_a['order']'";
         mysqli_query($con,$b);
         $i++;
     }
